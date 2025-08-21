b0c097cc2cdeb89d58eaa8c3d0b653c25b82275d
nirvanaaputri <nirvanaaputri@gmail.com>
2025-08-21 09:13:29 +0700

    add menu in homepage

--- app/src/main/java/com/example/labux_animedxd/HomeActivity.java
+++ app/src/main/java/com/example/labux_animedxd/HomeActivity.java
@@ -12,6 +12,14 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import android.content.SharedPreferences;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
 
 public class HomeActivity extends BaseActivity {
 
@@ -103,6 +111,30 @@ public class HomeActivity extends BaseActivity {
         navAbout.setOnClickListener(v -> {
             Intent intent3 = new Intent(HomeActivity.this, AboutUsActivity.class);
             startActivity(intent3);
+        });
+
+        ImageButton btnMenu = findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(v -> {
+            View popupView = getLayoutInflater().inflate(R.layout.popup_menu_custom, null);
+
+            PopupWindow popupWindow = new PopupWindow(popupView,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    true);
+
+            popupWindow.setOutsideTouchable(true);
+            popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            // Event klik logout
+            TextView tvLogout = popupView.findViewById(R.id.tvLogout);
+            tvLogout.setOnClickListener(view -> {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                popupWindow.dismiss();
+            });
+
+            popupWindow.showAsDropDown(v, 0, 0);
         });
 
     }
